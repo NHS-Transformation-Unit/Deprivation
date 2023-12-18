e4b028601265cf22e8fe0e5a97f718cec0c61e32
ASW-Analyst <andy.wilson8@nhs.net>
2023-12-18 17:53:22 +0000

    Add missing hline() function

--- src/app/visualisation/charts.R
+++ src/app/visualisation/charts.R
@@ -102,6 +102,20 @@ lsoa_decile_domain_comp_plot <- function(imd) {
     mutate(Decile = factor(Decile, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
   
   
+  hline <- function(y = 0, color = "black") {
+    list(
+      type = "line", 
+      x0 = 0, 
+      x1 = 1, 
+      xref = "paper",
+      y0 = y, 
+      y1 = y, 
+      line = list(color = color),
+      style = "dashed"
+    )
+  }
+  
+  
   domain_plot <- . %>% 
     plot_ly(x = ~Decile, y = ~Prop, fill = ~Metric_Tidy, type = "bar",
             hoverinfo = 'text', text = ~paste0(Metric_Tidy, " - Decile ", Decile, ": ",
